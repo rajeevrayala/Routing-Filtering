474072205dc2ed109765ad87eda2e7a220981594
Rajeev Rayala <rayalajava99@gmail.com>
2017-04-29 23:30:54 -0500

    Add files via upload

---
+++ springroutingfiltering/gateway/src/main/java/com/example/filters/pre/SimpleFilter.java
@@ -0,0 +1,37 @@
+package com.example.filters.pre;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Rajeev on 4/29/2017.
+ */
+public class SimpleFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+
+}
